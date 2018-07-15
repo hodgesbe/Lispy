df7c370dfaeab8586a8dca9346b80a6e76884602
hodgesbe <bhodges87@gmail.com>
2018-07-15 14:49:44 -0400

    Add mpc parser and support for simple Polish notation grammer.

--- repl.c
+++ repl.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc/mpc.h"
 
 /* Inludes both readline and history */
 #ifdef _WIN32
@@ -31,8 +32,27 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv)
 {
+    /* mpc Grammer Definition */
+
+    /* Create Parsers */
+    mpc_parser_t* Number   = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr     = mpc_new("expr");
+    mpc_parser_t* Lispy    = mpc_new("lispy");
+
+    /* Define with following language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                      \
+                number   : /-?[0-9]+/ ;                            \
+                operator : '+' | '-' | '*' | '/';                  \
+                expr     : <number> | '(' <operator> <expr>+ ')' ; \
+                lispy    : /^/ <operator> <expr>+ /$/ ;            \
+            ",
+            Number, Operator, Expr, Lispy);
+
+
     /* Print Version and Exit Information */
-    puts("Lispy Version 0.0.0.0.1");
+    puts("Lispy Version 0.0.0.0.2");
     puts("Press Ctrl+c to Exit\n");
 
     /* In a never ending loop */
@@ -44,12 +64,26 @@ int main(int argc, char** argv)
         /* Add input to history */
         add_history(input);
 
-        /* Echo input back to user */
-        printf("No you're a %s\n", input);
+        /* Try to parse input */
+        mpc_result_t r;
+
+        if (mpc_parse("<stdin>", input, Lispy, &r))
+        {
+            /* On Success Print the AST */
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else /* Print Error */
+        {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         /* Free retrieved input */
         free(input);
     }
+
+    /* Underfine and Delete our Parsers */
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
