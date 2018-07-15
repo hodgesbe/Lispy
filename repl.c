6d37ef6f442d9fe9106038c4d1076b8a57e6b5bf
hodgesbe <bhodges87@gmail.com>
2018-07-15 11:29:40 -0400

    Add preprocessor checks for portability

--- repl.c
+++ repl.c
@@ -2,7 +2,32 @@
 #include <stdlib.h>
 
 /* Inludes both readline and history */
+#ifdef _WIN32
+#include<string.h>
+
+static char buffer[2048];
+
+/* Custom readline function to use
+ * if running on Windows
+ */
+char* readline(char* prompt)
+{
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+/* Stub add_history function */
+void add_history(char* unused) {}
+
+/* If not on Windows */
+#else
+
 #include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv)
 {
